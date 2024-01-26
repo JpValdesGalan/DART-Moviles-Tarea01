9b07d44f76d3f1acab329d75500f4c14d200ae89
jpvaldesgalan <jpvaldesgalan@gmail.com>
2024-01-25 22:34:24 -0600

    change tarea

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Tarea 01 - Conociendo los Scaffold y widgets mas comunes
 
 ### Lo que mas me costo trabajo
-Lo primero que me costo fue terminar de entender como funciona flutter y como funcionan sus layouts a base de arboles, como lo llaman en la documentacion de flutter widget tree, ya siendo mas familiar a ese termino y como funciona me fue mas sencillo continuar con la tarea, unicamente tambien l igual que me sucedia con angular, la verdad batallo mucho con la documentacion de google, aunque para la de flutter siento que esta muy completo.
+
+Lo primero que me costo fue terminar de entender como funciona flutter y como funcionan sus layouts a base de arboles, como lo llaman en la documentacion de flutter widget tree, ya siendo mas familiar a ese termino y como funciona me fue mas sencillo continuar con la tarea, unicamente tambien l igual que me sucedia con angular, la verdad batallo mucho con la documentacion de google, aunque para la de flutter siento que esta mas completo.
 
 ### Lo que aprendi
+
 - Crear un proyecto de Flutter.
 - Flutter Widget Trees
 - Flutter Layout
 - Flutter States
 
-  ### Screenshots de la app
-  
+### Screenshots de la app
+
+![Screenshot01](<./screenshots/Screenshot 2024-01-25 185853.png>)
+![Screenshot02](<./screenshots/Screenshot 2024-01-25 222656.png>)
--- tarea01/lib/main.dart
+++ tarea01/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,58 +18,78 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.lightBlue,
         ),
         body: Container(
-            height: 200,
-            width: 500,
-            margin: const EdgeInsets.all(20),
-            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-            child: const Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.account_circle,
-                      size: 64,
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "Flutter McFlutter",
-                          style: TextStyle(
-                              fontSize: 32, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "Experienced App Developer",
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "123 Main Street",
-                    ),
-                    Text(
-                      "(415) 555-0198",
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Icon(Icons.accessibility_new),
-                    Icon(Icons.timer),
-                    Icon(Icons.phone_android_sharp),
-                    Icon(Icons.phone_android),
-                  ],
-                ),
-              ],
-            )),
+          height: 200,
+          width: 500,
+          margin: const EdgeInsets.all(20),
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.account_circle, size: 64),
+                  Column(
+                    children: [
+                      Text(
+                        "Flutter McFlutter",
+                        style: TextStyle(
+                            fontSize: 32, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Experienced App Developer",
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("123 Main Street"),
+                  Text("(415) 555-0198"),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CustomButtonIcon(icon: Icons.accessibility_new),
+                  CustomButtonIcon(icon: Icons.timer),
+                  CustomButtonIcon(icon: Icons.phone_android_sharp),
+                  CustomButtonIcon(icon: Icons.phone_android),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class CustomButtonIcon extends StatefulWidget {
+  final IconData icon;
+
+  const CustomButtonIcon({Key? key, required this.icon}) : super(key: key);
+
+  @override
+  State<CustomButtonIcon> createState() => _CustomButtonIconState();
+}
+
+class _CustomButtonIconState extends State<CustomButtonIcon> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(widget.icon),
+      color: isSelected ? Colors.indigo : Colors.black,
+      onPressed: () {
+        setState(() {
+          isSelected = !isSelected;
+        });
+      },
     );
   }
 }
